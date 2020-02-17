e7958e0282cdd50aeb36320e5137d93fdb23f5f7
AraqueGD <camiaraquecaro17@gmail.com>
2020-02-17 18:33:32 -0500

    [ADD]Tests

---
+++ tests/test_style.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import unittest
+import pep8
+
+
+class TestCodeFormat(unittest.TestCase):
+
+    def test_pep8_conformance(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['base_model.py', 'file_storage.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
