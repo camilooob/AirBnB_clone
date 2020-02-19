3202ed7033b663f5234f1c4bff0e0eed7682b52f
AraqueGD <camiaraquecaro17@gmail.com>
2020-02-18 19:33:05 -0500

    [ADD]More Classes

---
+++ models/city.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+from models.state import State
+
+
+class City(BaseModel):
+    state_id = ""
+    name = ""
---
+++ models/place.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+from models.city import City
+from models.state import State
+
+
+class Place(BaseModel):
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0
+    longitude = 0
+    amenity_ids = 0
