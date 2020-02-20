b4e94e64558921b9f7f0daace74a7611ffd2f19d
AraqueGD <camiaraquecaro17@gmail.com>
2020-02-19 20:05:26 -0500

    Fix]

--- tests/test_style_base_models.py
+++ tests/test_style_base_models.py
@@ -18,20 +18,3 @@ class Test_base_models(unittest.TestCase):
         """ Test Date Time """
         base1 = BaseModel()
         self.assertFalse(datetime.now() == base1.created_at)
-
-    def test_dict_date(self):
-        """ Test Dict Date """
-        base1 = BaseModel()
-        d = base1.to_dict()
-        self.assertEqual(d["__class__"], "BaseModel")
-        self.assertEqual(type(d["created_at"]), str)
-
-    def test_iso(self):
-        """ Test Format iso """
-        base1 = BaseModel()
-        cre = upd = datetime.now()
-        base1.created_at = cre
-        base1.updated_at = upd
-        d = base1.to_dict()
-        self.assertEqual(d["created_at"], cre.isoformat())
-        self.assertEqual(d["updated_at"], upd.isoformat())
